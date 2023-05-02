1f54d1289902b729a8976d0db910d11c5ea5f9d7
taoky <me@taoky.moe>
2023-05-03 00:21:55 +0800

    Improve LRC parse handling
    This modifies lrc_iter() in lyric/utils.rs
    - Move trimming logic from Lyric into_owned() to here
    - Sort lyrics by their timestamp after collecting lyrics vector. This helps handling LRC files with timestamp in malformed order

--- src/lyric/utils.rs
+++ src/lyric/utils.rs
@@ -2,13 +2,16 @@ use lrc_nom::{parse, LrcParseError};
 use std::time::Duration;
 
 pub fn lrc_iter<'a>(lyric: &'a str, lf: &str) -> Result<Vec<(&'a str, Duration)>, LrcParseError> {
-    Ok(parse(lyric, lf)?
+    let mut lrc_vec: Vec<_> = parse(lyric, lf)?
         .into_iter()
         .filter_map(|lrc_item| match lrc_item {
             lrc_nom::LrcItem::Metadata(_) => None,
             lrc_nom::LrcItem::Lyric(lyric, timestamp) => {
-                Some((lyric, Duration::from_millis(timestamp as u64)))
+                Some((lyric.trim(), Duration::from_millis(timestamp as u64)))
             }
         })
-        .collect())
+        .collect();
+    // handling malformed LRC timestamp by sorting them here
+    lrc_vec.sort_by(|(_, a), (_, b)| a.cmp(b));
+    Ok(lrc_vec)
 }
