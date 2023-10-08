963efc8d090d70ad9eca71a305c4915b519bc535
poly000 <34085039+poly000@users.noreply.github.com>
2023-10-09 00:21:38 +0800

    Update src/lib.rs
    Co-authored-by: taoky <me@taoky.moe>

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ pub const APP_ID: &str = "io.poly000.waylyrics";
 
 thread_local! {
     pub static CACHE_DIR: RefCell<String> = RefCell::new(String::new());
-        pub static THEME_PATH: RefCell<PathBuf> = RefCell::new(PathBuf::new());
+    pub static THEME_PATH: RefCell<PathBuf> = RefCell::new(PathBuf::new());
     pub static EXCLUDED_REGEXES: RefCell<RegexSet> = RefCell::new(RegexSet::empty());
     pub static MAIN_WINDOW: RefCell<Option<Window>> = RefCell::new(None);
 }
