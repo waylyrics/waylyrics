e5e3e69c8a7a6ad2fc520319fcc9f5cb78b54f8d
poly000 <1348292515@qq.com>
2024-01-10 22:57:23 +0800

    todo: use an external build system

--- src/sync/interop/mod.rs
+++ src/sync/interop/mod.rs
@@ -1,13 +1,18 @@
-mod mpris;
-
 pub mod acts;
 
+#[cfg(unix)]
+mod mpris;
+#[cfg(unix)]
 pub use mpris::{
-    clean_player,
-    connect_player_with_id,
-    hint_from_player,
-    list_player_names,
-    register_sync_task,
+    clean_player, connect_player_with_id, hint_from_player, list_player_names, register_sync_task,
+    update_lyric,
+};
+
+#[cfg(target_os = "windows")]
+mod smtc;
+#[cfg(target_os = "windows")]
+pub use smtc::{
+    clean_player, connect_player_with_id, hint_from_player, list_player_names, register_sync_task,
     update_lyric,
 };
 
