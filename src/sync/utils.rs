f226c774ac205f9f14551d34e9abe2a96b47212e
poly000 <1348292515@qq.com>
2024-02-19 22:40:47 +0800

    enhance: migrate to better dice-coefficient lib

--- src/sync/utils.rs
+++ src/sync/utils.rs
@@ -1,7 +1,7 @@
 use std::{path::PathBuf, time::Duration};
 
-use fuzzy_match::algorithms::{SimilarityAlgorithm, SorensenDice};
 use gtk::subclass::prelude::*;
+use sorensen::distance;
 
 use crate::log::trace;
 use crate::{app, lyric_providers::SongInfo};
@@ -43,7 +43,9 @@ pub fn match_likely_lyric<'a>(
             }
         }))
         .or_else(|| {
-            let mut fuzzy_match = SorensenDice::new();
+            let title: Vec<char> = title.chars().collect();
+            let album: Option<Vec<char>> = album.map(|a| a.chars().collect());
+            let singer: Option<Vec<char>> = singer.map(|s| s.chars().collect());
             search_result
                 .iter()
                 .map(
@@ -54,19 +56,21 @@ pub fn match_likely_lyric<'a>(
                          ..
                      }| {
                         let likelihood = fuzzy_match_song(
-                            &mut fuzzy_match,
-                            title,
-                            album,
-                            singer,
-                            r_title,
-                            r_album.as_deref(),
-                            r_singer,
+                            &title,
+                            album.as_deref(),
+                            singer.as_deref(),
+                            &r_title.chars().collect::<Vec<_>>(),
+                            r_album
+                                .as_ref()
+                                .map(|a| a.chars().collect::<Vec<_>>())
+                                .as_deref(),
+                            &r_singer.chars().collect::<Vec<_>>(),
                         );
                         trace!("p={likelihood} for {s:?}");
                         (s, likelihood)
                     },
                 )
-                .max_by_key(|(_, likelihood)| (likelihood * 128.) as u32)
+                .max_by_key(|(_, likelihood)| (likelihood * 1024.) as u32)
                 .map(|(s, _)| (s, 1))
         })
         .map(|(song, weight)| (song.id.as_str(), weight))
@@ -81,24 +85,23 @@ pub fn get_lyric_cache_path() -> Option<PathBuf> {
 }
 
 fn fuzzy_match_song(
-    fuzzy_match: &mut impl SimilarityAlgorithm,
-    title: &str,
-    album: Option<&str>,
-    singer: Option<&str>,
-    r_title: &str,
-    r_album: Option<&str>,
-    r_singer: &str,
+    title: &[char],
+    album: Option<&[char]>,
+    singer: Option<&[char]>,
+    r_title: &[char],
+    r_album: Option<&[char]>,
+    r_singer: &[char],
 ) -> f64 {
-    let title_likelihood = fuzzy_match.get_similarity(title, r_title) as f64;
-    let singer_likelihood = fuzzy_match.get_similarity(singer.unwrap_or_default(), r_singer) as f64;
+    let title_likelihood = distance(title, r_title);
+    let singer_likelihood = || distance(singer.unwrap_or_default(), r_singer) as f64;
     let album_likelihood =
-        fuzzy_match.get_similarity(album.unwrap_or_default(), r_album.unwrap_or_default()) as f64;
+        || distance(album.unwrap_or_default(), r_album.unwrap_or_default()) as f64;
     match (singer, album) {
         (Some(_), Some(_)) => {
-            title_likelihood * 0.6 + singer_likelihood * 0.3 + album_likelihood * 0.1
+            title_likelihood * 0.6 + singer_likelihood() * 0.3 + album_likelihood() * 0.1
         }
-        (Some(_), None) => title_likelihood * 0.7 + singer_likelihood * 0.3,
-        (None, Some(_)) => title_likelihood * 0.9 + album_likelihood * 0.1,
+        (Some(_), None) => title_likelihood * 0.7 + singer_likelihood() * 0.3,
+        (None, Some(_)) => title_likelihood * 0.9 + album_likelihood() * 0.1,
         (None, None) => title_likelihood,
     }
 }
