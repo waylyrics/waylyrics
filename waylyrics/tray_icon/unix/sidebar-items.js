103aa28a1c68813788c2c2bb3448a63810606af5
poly000 <poly000@users.noreply.github.com>
2024-02-21 22:30:06 +0000

    Automated deployment: Wed Feb 21 22:30:06 UTC 2024 a69d5a566304969639698db63870bf04fd1315ae

--- waylyrics/tray_icon/unix/sidebar-items.js
+++ waylyrics/tray_icon/unix/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["play_action","start_tray_service","ui_action"],"struct":["TrayIcon"]};
+window.SIDEBAR_ITEMS = {"fn":["play_action","restart_myself","start_tray_service","ui_action"],"struct":["TrayIcon"]};
