c3a631f3d33a3d4ef0798a8439b4288373cd6670
poly000 <1348292515a@gmail.com>
2024-04-03 23:06:57 +0800

    build: add icon for win32 build

---
+++ build.rs
@@ -0,0 +1,20 @@
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    if std::env::var("CARGO_CFG_TARGET_OS")?.as_str() == "windows" {
+        #[cfg(windows)]
+        {
+            use std::path::Path;
+            windows_exe_info::icon::icon_ico(Path::new("win_res/io.poly000.waylyrics.ico"));
+            windows_exe_info::versioninfo::VersionInfo::from_cargo_env_ex(
+                Some("Desktop lyric for SMTC"),
+                Some("Waylyrics"),
+                None,
+                None,
+            )
+            .link()?;
+        }
+    }
+
+    Ok(())
+}
