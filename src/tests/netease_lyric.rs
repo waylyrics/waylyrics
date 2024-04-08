b2a25beec01c31599fb4be15c85b55c94c046d19
poly000 <1348292515a@gmail.com>
2024-04-08 23:01:41 +0800

    chore(test): cleanup unused imports

--- src/tests/netease_lyric.rs
+++ src/tests/netease_lyric.rs
@@ -1,14 +1,15 @@
-use std::time::Duration;
-
-use crate::lyric_providers::netease::Netease;
-use crate::lyric_providers::{LyricLineOwned, LyricOwned, LyricParse, LyricProvider};
-
+#[cfg(feature = "net-test")]
 use anyhow::Result;
 
 /// This test cannot be run from offline environment!
 #[cfg(feature = "net-test")]
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 async fn get_netease_lyric() -> Result<()> {
+    use std::time::Duration;
+
+    use crate::lyric_providers::netease::Netease;
+    use crate::lyric_providers::{LyricLineOwned, LyricOwned, LyricParse, LyricProvider};
+
     let provider = Netease;
     let lyric_store = provider.query_lyric("708965").await?;
     let lyric = provider.parse_lyric(&lyric_store);
