4d02f5fc606a903d1309e6d4c3495b14d3be9584
poly000 <1348292515a@gmail.com>
2024-04-15 02:31:58 +0800

    chore: bump dependencies

--- src/lyric_providers/utils.rs
+++ src/lyric_providers/utils.rs
@@ -16,11 +16,14 @@ pub fn lrc_iter<'a>(
         .into_iter()
         .filter_map(|lrc_item| match lrc_item {
             lrc_nom::LrcItem::Metadata(_) => None,
-            lrc_nom::LrcItem::Lyric(lyric, timestamp) => Some(LyricLine {
-                text: lyric.trim(),
-                start_time: Duration::from_millis(timestamp as _),
-            }),
+            lrc_nom::LrcItem::Lyric(lyric, timestamps) => {
+                Some(timestamps.into_iter().map(|timestamp| LyricLine {
+                    text: lyric.trim(),
+                    start_time: Duration::from_millis(timestamp as _),
+                }))
+            }
         })
+        .flatten()
         .collect();
     // handling malformed LRC timestamp by sorting them here
     lrc_vec.sort_by_key(|line| line.start_time);
