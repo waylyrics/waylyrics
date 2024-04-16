e73f1e86e76a526138d02a76483d3989d4a5960b
poly000 <1348292515a@gmail.com>
2024-04-17 02:56:55 +0800

    docs(build): gettext-rs on windows cannot builds out-of-box with MSVC

--- build.ps1
+++ build.ps1
@@ -1,2 +1,6 @@
 $Env:WAYLYRICS_THEME_PRESETS_DIR = "..\share\waylyrics\themes"
+# to build i18n, you may have to switch to msys environment
+# I tried to not use `--force-local` for tar but gettext-sys
+# failed to build with MSVC, and I don't know how to write a
+# pkg-config file in gtk-build\gtk\x64\release\lib\pkgconfig
 cargo build --release --no-default-features -F tray-icon
