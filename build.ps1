50b2d355db489fab8d70f4056900f666e9079e6c
poly000 <1348292515a@gmail.com>
2024-04-17 03:25:18 +0800

    build: enable i18n for msvc build

--- build.ps1
+++ build.ps1
@@ -1,6 +1,3 @@
 $Env:WAYLYRICS_THEME_PRESETS_DIR = "..\share\waylyrics\themes"
-# to build i18n, you may have to switch to msys environment
-# I tried to not use `--force-local` for tar but gettext-sys
-# failed to build with MSVC, and I don't know how to write a
-# pkg-config file in gtk-build\gtk\x64\release\lib\pkgconfig
-cargo build --release --no-default-features -F tray-icon
+$Env:GETTEXT_DIR = "C:\gtk-build\gtk\x64\release"
+cargo build --release --no-default-features -F tray-icon -F i18n
