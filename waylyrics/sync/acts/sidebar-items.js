979902cfecebed4315dec6683ec9764218ece2f8
mokurin000 <mokurin000@users.noreply.github.com>
2024-04-25 12:03:21 +0000

    Automated deployment: Thu Apr 25 12:03:21 UTC 2024 e1c4e12b5ca8ab017214ffc219a2740c600052ca

--- waylyrics/sync/acts/sidebar-items.js
+++ waylyrics/sync/acts/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["register_connect","register_disconnect","register_refetch_lyric","register_remove_lyric","register_search_lyric"],"mod":["event"]};
+window.SIDEBAR_ITEMS = {"fn":["register_connect","register_disconnect","register_refetch_lyric","register_reload_lyric","register_remove_lyric","register_search_lyric"],"mod":["event"]};
