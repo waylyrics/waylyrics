225e043f129d882d656d425c5bd7cdfff0ed3caa
poly000 <1348292515a@gmail.com>
2024-05-24 16:07:37 +0800

    build: enable import lyric feature for windows

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 $Env:WAYLYRICS_THEME_PRESETS_DIR = "..\share\waylyrics\themes"
 $Env:GETTEXT_DIR = "C:\gtk-build\gtk\x64\release"
-cargo build --release --no-default-features -F tray-icon -F i18n
+cargo build --release --no-default-features -F tray-icon -F i18n -F import-lyric
