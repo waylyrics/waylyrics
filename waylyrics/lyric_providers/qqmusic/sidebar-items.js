eb07fa998b5243f147c9bca96b71739dc4888b12
mokurin000 <mokurin000@users.noreply.github.com>
2024-11-06 07:43:39 +0000

    Automated deployment: Wed Nov  6 07:43:39 UTC 2024 ad6ff17ee33ccbc71cbc19b4740d77ffb343d69a

--- waylyrics/lyric_providers/qqmusic/sidebar-items.js
+++ waylyrics/lyric_providers/qqmusic/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Error"],"fn":["get_songmid","verify_lyric"],"static":["QQMUSIC_API_CLIENT"],"struct":["QQMusic"]};
+window.SIDEBAR_ITEMS = {"enum":["Error"],"fn":["get_songmid","verify_lyric"],"mod":["typo"],"static":["QQMUSIC_API_CLIENT"],"struct":["QQMusic","QQMusicConfig"]};
