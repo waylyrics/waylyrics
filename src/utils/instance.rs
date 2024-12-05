e3c1f074361bf275f55af70fcf5ac439430cc924
poly000 <1348292515@qq.com>
2024-12-05 21:20:31 +0800

    fix: check if instance name used

--- src/utils/instance.rs
+++ src/utils/instance.rs
@@ -2,14 +2,13 @@ use anyhow::Result;
 
 #[cfg(unix)]
 pub fn acquire_instance_name() -> Result<()> {
+    use tracing::info;
+
     #[cfg(feature = "multi-monitor")]
     {
-        use std::hash::Hasher;
         use std::time::Duration;
 
         use dbus::blocking::Connection;
-        use gtk::glib::ffi::g_random_int_range;
-        use hex_simd::AsOut;
 
         let conn = Connection::new_session()?;
         let proxy = conn.with_proxy(
@@ -27,18 +26,13 @@ pub fn acquire_instance_name() -> Result<()> {
         if !waylyrics_instances.contains(crate::APP_ID_FIXED) {
             let _ = crate::INSTANCE_NAME.set(String::from(crate::APP_ID_FIXED));
         } else {
-            let random_num = unsafe { g_random_int_range(i32::MIN, i32::MAX) };
-            let mut hasher = ahash::AHasher::default();
-            hasher.write_i32(random_num);
-            let hash = hasher.finish();
-            let mut out: Vec<u8> = vec![0; 16];
-            let hash_str = hex_simd::encode_as_str(
-                &hash.to_ne_bytes(),
-                out.as_out(),
-                hex_simd::AsciiCase::Lower,
-            );
-            let instance_name = format!("{}-{}", crate::APP_ID_FIXED, hash_str);
-            let _ = crate::INSTANCE_NAME.set(instance_name);
+            loop {
+                let instance_name = gen_instance_name();
+                if !waylyrics_instances.contains(instance_name.as_str()) {
+                    let _ = crate::INSTANCE_NAME.set(instance_name);
+                    break;
+                }
+            }
         }
     }
     #[cfg(not(feature = "multi-monitor"))]
@@ -46,11 +40,35 @@ pub fn acquire_instance_name() -> Result<()> {
         let _ = crate::INSTANCE_NAME.set(String::from(crate::APP_ID_FIXED));
     }
 
+    info!("instance name: {}", crate::INSTANCE_NAME.get().unwrap());
     Ok(())
 }
 
 #[cfg(not(unix))]
 pub fn acquire_instance_name() -> Result<()> {
     let _ = crate::INSTANCE_NAME.set(String::from(crate::APP_ID_FIXED));
+    info!("instance name: {}", crate::INSTANCE_NAME.get().unwrap());
     Ok(())
+}
+
+#[cfg(feature = "multi-monitor")]
+fn gen_instance_name() -> String {
+    use std::hash::Hasher;
+
+    use gtk::glib::ffi::g_random_int_range;
+    use hex_simd::AsOut;
+
+    let random_num = unsafe { g_random_int_range(i32::MIN, i32::MAX) };
+    let mut hasher = ahash::AHasher::default();
+    hasher.write_i32(random_num);
+    let hash = hasher.finish();
+    let mut out: Vec<u8> = vec![0; 16];
+    let hash_str = hex_simd::encode_as_str(
+        &hash.to_ne_bytes(),
+        out.as_out(),
+        hex_simd::AsciiCase::Lower,
+    );
+    let (hash_str, _) = hash_str.split_at(hash_str.len() / 2);
+    let instance_name = format!("{}._{}", crate::APP_ID_FIXED, hash_str);
+    instance_name
 }
