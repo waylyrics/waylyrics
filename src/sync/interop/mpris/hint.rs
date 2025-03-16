112a5a79e5a596f8e5db833db5d937c0c2cc9a94
wind_mask <wind_mask_proton@proton.me>
2025-03-16 22:24:25 +0800

    feat: 添加对从音乐文件元数据读取歌词的支持

--- src/sync/interop/mpris/hint.rs
+++ src/sync/interop/mpris/hint.rs
@@ -4,7 +4,7 @@ use crate::lyric_providers::netease::Netease;
 use crate::lyric_providers::qqmusic::QQMusic;
 
 use crate::sync::interop::mpris::PLAYER;
-use crate::sync::lyric::fetch::tricks::get_lrc_path;
+use crate::sync::lyric::fetch::tricks::{get_lrc_path, lyric_tag_exists};
 use crate::sync::lyric::fetch::LyricHint;
 use crate::sync::ENABLE_LOCAL_LYRIC;
 
@@ -63,6 +63,10 @@ pub fn hint_from_player() -> Option<LyricHint> {
                     let music_path = url::Url::from_str(meta_url)
                         .ok()
                         .and_then(|music_uri| music_uri.to_file_path().ok())?;
+                    if lyric_tag_exists(&music_path) {
+                        return Some(LyricHint::LyricMetadata(music_path));
+                    }
+
                     get_lrc_path(music_path)
                         .filter(|lyric_path| lyric_path.exists())
                         .map(LyricHint::LyricFile)
