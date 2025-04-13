0ab82f5009bbb4c050d9c4c9fbdb46a3c6a53259
mokurin000 <1348292515a@gmail.com>
2025-04-13 21:11:05 +0800

    fix(windows): build script

--- build.rs
+++ build.rs
@@ -3,8 +3,7 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     #[cfg(windows)]
     if std::env::var("CARGO_CFG_TARGET_OS")?.as_str() == "windows" {
-        use std::path::Path;
-        windows_exe_info::icon::icon_ico(Path::new("res/win/icon.ico"));
+        windows_exe_info::icon::icon_ico("res/win/icon.ico");
         windows_exe_info::versioninfo::VersionInfo::from_cargo_env_ex(
             Some("Desktop lyric for SMTC"),
             Some("Waylyrics"),
