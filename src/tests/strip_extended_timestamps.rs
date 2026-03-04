fc79bce8c72612b4d8bb6739130e4b210197c930
莯凛 <mokurin000@gmail.com>
2026-03-04 19:08:27 +0800

    test: add full test for stripping single extra tag

--- src/tests/strip_extended_timestamps.rs
+++ src/tests/strip_extended_timestamps.rs
@@ -3,13 +3,62 @@ use std::borrow::Cow;
 use crate::lyric_providers::strip_extended_timestamps;
 
 #[test]
-fn test_strip_extended_timestamps() {
-    let lrc = "[00:12.34]Hello <01:23.456>world!";
-    let stripped = strip_extended_timestamps(lrc);
-    assert_eq!(stripped, "[00:12.34]Hello world!");
-
+fn test_direct_borrow() {
     // 没有尖括号，直接借用
-    let lrc2 = "Hello world!";
-    let stripped2 = strip_extended_timestamps(lrc2);
+    let lrc = "[00:00.00]Hello world!";
+    let stripped2 = strip_extended_timestamps(lrc);
     assert!(matches!(stripped2, Cow::Borrowed(_)));
+}
+
+#[test]
+fn test_ill_form_ignored() {
+    let lrc = "[00:12.34]Hello <01:23:456>world!";
+    let stripped = strip_extended_timestamps(lrc);
+    assert_eq!(stripped, "[00:12.34]Hello <01:23:456>world!");
+}
+
+#[test]
+fn test_not_tag_ignored() {
+    let lrc = "[00:12.34]<FM> 纯音乐";
+    let stripped = strip_extended_timestamps(lrc);
+    assert_eq!(stripped, "[00:12.34]<FM> 纯音乐");
+}
+
+#[test]
+fn test_all_possible_case() {
+    let answer = "[00:00.00]你好";
+    for min in (0..100)
+        .map(|num| {
+            if num < 10 {
+                vec![format!("{num}"), format!("{num:02}")]
+            } else {
+                vec![format!("{num}")]
+            }
+        })
+        .flatten()
+    {
+        for sec in (0..60)
+            .map(|num| {
+                if num < 10 {
+                    vec![format!("{num}"), format!("{num:02}")]
+                } else {
+                    vec![format!("{num}")]
+                }
+            })
+            .flatten()
+        {
+            for subsec in 1..1000 {
+                if subsec < 10 {
+                    let lrc_line1 = format!("[00:00.00]你<{min}:{sec}.{subsec}>好");
+                    assert_eq!(strip_extended_timestamps(&lrc_line1), answer,);
+                }
+                if subsec < 100 {
+                    let lrc_line2 = format!("[00:00.00]你<{min}:{sec}.{subsec:02}>好");
+                    assert_eq!(strip_extended_timestamps(&lrc_line2), answer,);
+                }
+                let lrc_line3 = format!("[00:00.00]你<{min}:{sec}.{subsec:03}>好");
+                assert_eq!(strip_extended_timestamps(&lrc_line3), answer,);
+            }
+        }
+    }
 }
