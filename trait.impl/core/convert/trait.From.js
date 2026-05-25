630ee618c7e84d176f7a91506cf339db6a839e24
mokurin000 <mokurin000@users.noreply.github.com>
2026-05-25 07:52:48 +0000

    Automated deployment: Mon May 25 07:52:48 UTC 2026 2e7437f5834ed22cfd16718e9cecb4aedefb3782

--- trait.impl/core/convert/trait.From.js
+++ trait.impl/core/convert/trait.From.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["waylyrics",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"waylyrics/config/enum.Align.html\" title=\"enum waylyrics::config::Align\">Align</a>&gt; for Align",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"waylyrics/config/enum.Align.html\" title=\"enum waylyrics::config::Align\">Align</a>&gt; for Justification",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;Error&gt; for <a class=\"enum\" href=\"waylyrics/utils/enum.ParseError.html\" title=\"enum waylyrics::utils::ParseError\">ParseError</a>",0]]]]);
+    const implementors = Object.fromEntries([["waylyrics",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"waylyrics/config/enum.Align.html\" title=\"enum waylyrics::config::Align\">Align</a>&gt; for Align",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"waylyrics/config/enum.Align.html\" title=\"enum waylyrics::config::Align\">Align</a>&gt; for Justification",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;Error&gt; for <a class=\"enum\" href=\"waylyrics/utils/enum.ParseError.html\" title=\"enum waylyrics::utils::ParseError\">ParseError</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;Metadata&gt; for <a class=\"struct\" href=\"waylyrics/sync/struct.TrackMeta.html\" title=\"struct waylyrics::sync::TrackMeta\">TrackMeta</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[855]}
+//{"start":59,"fragment_lengths":[1145]}
