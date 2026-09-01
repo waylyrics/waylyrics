a6763394d46241ecc9655d59723dc2f642d196d2
mokurin000 <mokurin000@users.noreply.github.com>
2026-09-01 00:33:59 +0000

    Automated deployment: Tue Sep  1 00:33:59 UTC 2026 481898d465b5c8a7691a177a03fd4bfed572a45a

--- trait.impl/documented/trait.DocumentedFields.js
+++ trait.impl/documented/trait.DocumentedFields.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["waylyrics",[["impl DocumentedFields for <a class=\"struct\" href=\"waylyrics/config/struct.Config.html\" title=\"struct waylyrics::config::Config\">Config</a>",0],["impl DocumentedFields for <a class=\"struct\" href=\"waylyrics/config/struct.Triggers.html\" title=\"struct waylyrics::config::Triggers\">Triggers</a>",0],["impl DocumentedFields for <a class=\"struct\" href=\"waylyrics/lyric_providers/qqmusic/struct.QQMusicConfig.html\" title=\"struct waylyrics::lyric_providers::qqmusic::QQMusicConfig\">QQMusicConfig</a>",0]]]]);
+    const implementors = Object.fromEntries([["waylyrics",[["impl DocumentedFields for <a class=\"struct\" href=\"waylyrics/config/struct.Config.html\" title=\"struct waylyrics::config::Config\">Config</a>",0],["impl DocumentedFields for <a class=\"struct\" href=\"waylyrics/lyric_providers/qqmusic/struct.QQMusicConfig.html\" title=\"struct waylyrics::lyric_providers::qqmusic::QQMusicConfig\">QQMusicConfig</a>",0],["impl DocumentedFields for <a class=\"struct\" href=\"waylyrics/config/struct.Triggers.html\" title=\"struct waylyrics::config::Triggers\">Triggers</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
